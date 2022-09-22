b7ee8ab6fc06eb02ca347ced9a6928b91de3a91c
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-23 01:07:53 +0530

    Create  Swaping.java

---
+++ Swaping.java
@@ -0,0 +1,16 @@
+public class Swaping{
+    public static void main(String[] args){
+        int first = 5;
+        int second = 7;
+
+
+        // swaping temporary
+
+        int temp = first;
+        first = second;
+        second = temp;
+
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
