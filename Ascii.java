0e054db0bf6146e1f7eb13cfd3c2089a54dc364c
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-23 01:07:11 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+public class Ascii {
+
+    public static void main(String[] args){
+        char ch = 'a';
+        int assci = ch;
+        int castAscci = (int) ch;
+
+        System.out.println(ch +" " + assci);
+        System.out.println(ch +" " + castAscci);
+    }
+    
+}
