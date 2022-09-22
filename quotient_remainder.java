33873c6da12c5db280a95949d6d752b901734536
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-23 01:11:49 +0530

    Create quotient_remainder.java

---
+++ quotient_remainder.java
@@ -0,0 +1,13 @@
+public class quotient_remainder {
+    public static void main(String[] args) {
+        int divident = 11;
+        int divisor = 2;
+
+        int quotant = divident / divisor;
+        int remainder = divident % divisor;
+
+        System.out.println(quotant);
+        System.out.println(remainder);
+    }
+    
+}
