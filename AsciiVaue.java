4b5eaeb986b4b77abbe62dc9f9faf481fd49bd47
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-23 00:50:39 +0530

    Create AsciiVaue.java

---
+++ AsciiVaue.java
@@ -0,0 +1,13 @@
+package Question_Practice;
+//rint ASCII values and their equivalent characters. ASCII value vary from 0 to 255.
+public class AsciiVaue {
+    public static void main(String[] args){
+    char c = 'a';
+    int AsciiVaue = c;
+    System.out.println("AsciiVaue"+AsciiVaue);
+
+    int n = 255;
+    char Asciival =(char) n;
+    System.out.println(Asciival);
+    }
+}
